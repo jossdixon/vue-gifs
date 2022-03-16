a5556d63831404fe3625dafb2c608681f802c428
Joss Dixon <josscdixon@gmail.com>
2022-03-16 14:46:22 +0000

    app component

--- App.vue
+++ App.vue
@@ -1,17 +1,39 @@
 <template>
-  <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
-  </view>
+    <view>
+        <scroll-view class="scroll-view">
+            <!-- TODO: Create gif item and header -->
+            <view class="loading-container" :style="{flex: 1, justifyContent: 'center'}" v-if="loading">
+              <activity-indicator size="large" color="#0000ff"></activity-indicator>
+            </view>
+        </scroll-view>
+    </view>
 </template>
 
+<script>
+  import Giphy from 'giphy-js-sdk-core';
+  const client = Giphy('GIPHY_API_KEY');
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+        gifs: [],
+        loading: true,
+      };
+    },
+    async created() {
+      const response = await client.trending('gifs', {limit: 20});
+      this.gifs = response.data;
+    },
+  };
+</script>
+
 <style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
+    .scroll-view {
+        padding-top: 20px;
+        padding-bottom: 30px;
+    }
+    .loading-container {
+        height: 600px;
+    }
 </style>
